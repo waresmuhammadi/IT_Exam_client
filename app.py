893328ef3c663173c69da23a31024df365db3c5e
wares <mw@tibxtech.com>
2025-10-04 16:40:08 +0430

    latest

--- app.py
+++ app.py
@@ -1,11 +1,19 @@
 from flask import Flask, render_template, request, jsonify
+import requests
 
 app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return jsonify({"message": "Hello World", "Application": "Team Application"})
+    return render_template('index.html')
 
+
+
+@app.route("/get_authentication_accounts")
+def get_authentication_accounts():
+    url = "http://127.0.0.1:8000/authentication"
+    response = requests.get(url)
+    return jsonify(response.json())
 
 
 if __name__ == '__main__':
